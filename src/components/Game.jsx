3ec84f571b5acc0b49f34969930134d48b3cc6bf
Pedro Silva <pedro.rls93@gmail.com>
2019-03-09 13:08:27 -0300

    time travel

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -31,6 +31,16 @@ export default class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map(( step, move ) => {
+      const desc = move ? 'Go to move #' + move : 'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{ desc }</button>
+        </li>
+      );
+    });
+
     let status;
 
     if (winner) {
@@ -49,7 +59,7 @@ export default class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{ status }</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{ moves }</ol>
         </div>
       </div>
     );
