fda387a558d75af5278de4b3116d2005c0b02208
Pedro Silva <pedro.rls93@gmail.com>
2019-03-08 23:03:29 -0300

    taking turns

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,12 +4,16 @@ import Square from './Square';
 export default class Board extends React.Component {
   state = {
     squares: Array(9).fill(null),
+    xIsNext: true,
   };
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares });
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ 
+      squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
   
   renderSquare(i){
@@ -22,7 +26,7 @@ export default class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
 
     return (
       <div>
